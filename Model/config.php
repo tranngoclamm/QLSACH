a833ce5ed71c899eb7c96d4b0ddb12be0fa35a1f
tranngoclamm <124114520+tranngoclamm@users.noreply.github.com>
2024-05-10 15:50:53 +0700

    Initial database

---
+++ Model/config.php
@@ -0,0 +1,20 @@
+<?php
+$uri = "mysql://avnadmin:AVNS_HxJ0KL7R6R9aGqhSh7n@mysql-6c78cd5-db25ts4.i.aivencloud.com:27943/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];
+$conn .= ";dbname=QUANLYSACH";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+    exit();
+}
+?>
