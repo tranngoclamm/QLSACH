250b2660f3d0577a925e5a849c02349d571e6850
tranngoclamm <124114520+tranngoclamm@users.noreply.github.com>
2024-05-11 12:21:24 +0000

    Update README.md

--- View/Sach.php
+++ View/Sach.php
@@ -4,6 +4,7 @@ echo '<h1>Thông tin sách</h1>';
 foreach ($books as $book) {
     echo 'Mã sách: ' . $book['MaSach'] . '<br>';
     echo 'Tên sách: ' . $book['TenSach'] . '<br>';
+    echo 'Số lượng: ' . $book['SoLuong'] . '<br>';
     echo '------------------------<br>';
 }
 ?>
