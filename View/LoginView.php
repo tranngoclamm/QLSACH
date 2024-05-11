e3ff33a8c590d39fe2c885d44452075610007d2a
your name <21010291@st.phenikaa-uni.edu.vn>
2024-05-12 01:29:24 +0700

    Modify LoginView

--- View/LoginView.php
+++ View/LoginView.php
@@ -12,4 +12,6 @@ class LoginView {
         ';
     }
 }
+$LoginView  = new LoginView();
+$LoginView->showLogin();
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
+header("Location: View/LoginView.php");
+exit;
 ?>
-
-<html>
-	<head>
-		<title>Advanced Web Development K15-N03</title>
-	</head>
-	<body>
-		<?php 
-		sayHello('K15-N03 2024');		
-		?>
-		<form action="checkLogin.php" method="POST">
-		<a>Username:</a><input type=text name="userName" size =16>
-		<a>Password:</a><input type=password name="passWord" size =16>
-		<input type=submit name=submit value="Login">
-		</form>
-	</body>
-</html>
